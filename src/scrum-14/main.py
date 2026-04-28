64c2782bd5d0be5733510d8b23d0c77a869020b2
GitNida <154496725+GitNida@users.noreply.github.com>
2026-04-28 17:08:28 +0530

    feat(SCRUM-14): add main.py

---
+++ src/scrum-14/main.py
@@ -0,0 +1,20 @@
+"""
+Main implementation module for SCRUM-14.
+This module contains the main function to display the message "wow! we have done it :)".
+"""
+
+def display_message():
+    """
+    Function to display the message "wow! we have done it :)".
+
+    Returns:
+        str: The message to be displayed.
+    """
+    message = "wow! we have done it :)"
+    print(message)
+    return message
+
+
+if __name__ == "__main__":
+    # Entry point of the program
+    display_message()
