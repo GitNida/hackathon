f0bde8d884ce2877fc871ff94486ef4a3d23ada5
GitNida <154496725+GitNida@users.noreply.github.com>
2026-04-28 17:03:49 +0530

    feat(SCRUM-14): add main.py

---
+++ src/scrum-14/main.py
@@ -0,0 +1,11 @@
+def display_message():
+    """
+    Function to display the success message.
+    """
+    message = "wow! we have done it :)"
+    print(message)
+    return message
+
+if __name__ == "__main__":
+    # Entry point for the program
+    display_message()
